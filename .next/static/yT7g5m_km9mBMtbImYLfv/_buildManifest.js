4e8cbbf0e9b74ce776f111257924eb60d9450f54
Hieu Nguyen <hieunguyenel@gmail.com>
2021-09-13 17:44:16 +0700

    shopify extension boilerplate and prisma integrate

--- .next/static/yT7g5m_km9mBMtbImYLfv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-60c98917457c356a0037.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5bdfe767480352e01bfa.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
